1b455c874d20f7850ab7d2f3e3d6ff34844c314d
17sTomy <tomasignacioalv@hotmail.com>
2024-10-11 20:12:00 -0300

    fix: add auth for getting movies

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,41 +1,24 @@
-import axios from 'axios';
+import { AuthorizedService } from '../api';
 
-const BASE_URL = 'http://localhost:8081';
-
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b21hcyt0ZXN0aW5nQGdtYWlsLmNvbSIsImlhdCI6MTcyODY4MDA3OSwiZXhwIjoxNzI4NjgzNjc5fQ.hYzMCfp508AbEghPWQkaHbE1tOULH5wYHwGFLk5fHtY";
+const BASE_URL = 'api/products';
 
 export const getAllProducts = async () => {
-  const response = await axios.get(`${BASE_URL}/api/products`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await AuthorizedService.get(BASE_URL);
   return response.data;
 };
 
 export const getFeaturedProducts = async () => {
-  const response = await axios.get(`${BASE_URL}/api/products/featured`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await AuthorizedService.get(`${BASE_URL}/featured`);
   return response.data;
 };
 
 export const getProductsByCategory = async (category) => {
-  const response = await axios.get(`${BASE_URL}/api/products/category/${category}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await AuthorizedService.get(`${BASE_URL}/category/${category}`);
   return response.data;
 };
 
 export const getViewedProducts = async () => {
-  const response = await axios.get(`${BASE_URL}/api/products/recently-viewed`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await AuthorizedService.get(`${BASE_URL}/recently-viewed`);
+  console.log(response, "aaa")
   return response.data;
 };
