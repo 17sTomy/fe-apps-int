0dfb3137f6ba01e66c71c2f20ac25f63e6617c3e
17sTomy <tomasignacioalv@hotmail.com>
2024-10-11 18:33:43 -0300

    feat: added buttons for each section

---
+++ src/components/Product/AllProductsSection.jsx
@@ -0,0 +1,31 @@
+import useProducts from '../../hooks/useProducts';
+import ProductCard from './ProductCard';
+import { Box } from '@mui/material'; 
+
+export const AllProductsSection = () => {
+  const { products, loading, error } = useProducts();
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error fetching products: {error.message}</p>}
+
+      {!loading && !error && (
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: 2, 
+            justifyContent: 'center'
+          }}
+        >
+          {products.map((product) => (
+            <Box key={product.id} >
+              <ProductCard product={product} />
+            </Box>
+          ))}
+        </Box>
+      )}
+    </>
+  );
+};
