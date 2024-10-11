1be68c4c18568ea4901397a1199784708ae28ed4
17sTomy <tomasignacioalv@hotmail.com>
2024-10-11 18:39:24 -0300

    feat: functions for getting viewed products, featured and by cateogry

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -2,10 +2,37 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8081';
 
+const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b21hcyt0ZXN0aW5nQGdtYWlsLmNvbSIsImlhdCI6MTcyODY4MDA3OSwiZXhwIjoxNzI4NjgzNjc5fQ.hYzMCfp508AbEghPWQkaHbE1tOULH5wYHwGFLk5fHtY";
+
 export const getAllProducts = async () => {
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b21hcyt0ZXN0aW5nQGdtYWlsLmNvbSIsImlhdCI6MTcyODY3MjQzNywiZXhwIjoxNzI4Njc2MDM3fQ.cLyS29NvBE7fvRux5Wz-ndmpRDaOjctJNTVwpq3GUik";
-  
   const response = await axios.get(`${BASE_URL}/api/products`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const getFeaturedProducts = async () => {
+  const response = await axios.get(`${BASE_URL}/api/products/featured`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const getProductsByCategory = async (category) => {
+  const response = await axios.get(`${BASE_URL}/api/products/category/${category}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const getViewedProducts = async () => {
+  const response = await axios.get(`${BASE_URL}/api/products/recently-viewed`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
