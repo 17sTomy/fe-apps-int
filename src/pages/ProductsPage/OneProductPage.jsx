80fa76ae52f47ebeb7836d4a3ae86a063e792e64
17sTomy <tomasignacioalv@hotmail.com>
2024-10-15 20:49:59 -0300

    feat: new section for showing one product

---
+++ src/pages/ProductsPage/OneProductPage.jsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from '../../template/DashboardLayout/DashboardLayout';
+import { OneProductSection } from '../../components/Product/OneProductSection';
+
+export const OneProductPage = () => {
+
+  return (
+    <DashboardLayout>
+      <OneProductSection />
+    </DashboardLayout>
+  );
+};
