0e9e745149b435430e04b7901382a33ba18b705b
matiasgluck <matiasugluck@gmail.com>
2024-10-15 13:59:43 -0300

    feat: public products

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,24 +1,24 @@
-import { AuthorizedService } from '../api';
+import { AuthorizedService, UnauthorizedService } from '../api';
 
 const BASE_URL = 'api/products';
 
 export const getAllProducts = async () => {
-  const response = await AuthorizedService.get(BASE_URL);
+  const response = await UnauthorizedService.get(BASE_URL);
   return response.data;
 };
 
 export const getFeaturedProducts = async () => {
-  const response = await AuthorizedService.get(`${BASE_URL}/featured`);
+  const response = await UnauthorizedService.get(`${BASE_URL}/featured`);
   return response.data;
 };
 
 export const getProductsByCategory = async (category) => {
-  const response = await AuthorizedService.get(`${BASE_URL}/category/${category}`);
+  const response = await UnauthorizedService.get(`${BASE_URL}/category/${category}`);
   return response.data;
 };
 
 export const getViewedProducts = async () => {
   const response = await AuthorizedService.get(`${BASE_URL}/recently-viewed`);
-  console.log(response, "aaa")
+  console.log(response, 'aaa');
   return response.data;
 };
