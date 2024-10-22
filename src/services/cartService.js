be8d2e8ec88e4ef408145b7f7f406828ebcbb7d5
17sTomy <tomasignacioalv@hotmail.com>
2024-10-22 19:29:12 -0300

    feat: add to cart function in services

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -4,5 +4,10 @@ const BASE_URL = '/api/cart';
 
 export const fetchCart = async () => {
   const response = await AuthorizedService.get(`${BASE_URL}`);
+  console.log(response.data)
   return response.data;
+};
+
+export const addToCart = async (data) => {
+  await AuthorizedService.post(`${BASE_URL}/add`, data);
 };
