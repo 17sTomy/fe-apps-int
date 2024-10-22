2e29f7b0209c777e0341690f6b6f8e13b8939076
17sTomy <tomasignacioalv@hotmail.com>
2024-10-22 20:28:00 -0300

    feat: add functions to remove from cart, remove one and clear cart in services

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -11,3 +11,15 @@ export const fetchCart = async () => {
 export const addToCart = async (data) => {
   await AuthorizedService.post(`${BASE_URL}/add`, data);
 };
+
+export const removeFromCart = async (id) => {
+  await AuthorizedService.delete(`${BASE_URL}/remove/${id}`);
+};
+
+export const removeOneFromCart = async (id) => {
+  await AuthorizedService.delete(`${BASE_URL}/reduce/${id}`);
+};
+
+export const clearCart = async (id) => {
+  await AuthorizedService.delete(`${BASE_URL}/clear`);
+};
