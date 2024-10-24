49f2d91245874a2c5b93a0007fe7df9a1d3d6c03
matiasgluck <matiasugluck@gmail.com>
2024-10-24 12:02:13 -0300

    feat: image carousel into products

--- src/components/Product/OneProductSection.jsx
+++ src/components/Product/OneProductSection.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import useProducts from '../../hooks/useProducts';
 import Loader from '../common/Loader/Loader';
-import { getProduct, viewProduct } from '../../services/productsService';
+import { getAllImages, getProduct, viewProduct } from '../../services/productsService';
 import { useTheme } from '../../hooks/useTheme';
 import { Box, IconButton, Typography, Button } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import CartButtonInPage from '../Cart/CartButtonInPage';
+import { ImageModifier } from './ImageModifier';
+import Carousel from 'react-material-ui-carousel';
+import * as React from 'react';
 
 export const OneProductSection = () => {
   const { id } = useParams();
@@ -15,11 +18,22 @@ export const OneProductSection = () => {
   const { products, loading, error } = useProducts(getProduct, id);
   const navigate = useNavigate();
 
+  const [images, setImages] = useState([]);
+
+  const fetchAllImages = async () => {
+    const images = await getAllImages(products.id);
+    setImages([{ url: products.imageUrl }, ...images]);
+  };
+
   useEffect(() => {
     if (id) {
       viewProduct(id);
     }
   }, [id]);
+
+  useEffect(() => {
+    if (products.id) fetchAllImages();
+  }, [products]);
 
   return (
     <>
@@ -36,26 +50,28 @@ export const OneProductSection = () => {
             padding: '20px',
             width: '100%',
             margin: 'auto',
-          }}
-        >
+          }}>
           <Button
             startIcon={<ArrowBackIosNewIcon />}
             onClick={() => navigate(-1)}
-            sx={{ alignSelf: 'flex-start', marginTop: '-20px' }}
-          >
+            sx={{ alignSelf: 'flex-start', marginTop: '-20px' }}>
             Volver
           </Button>
-          <Box
-            component="img"
-            src={products.imageUrl}
-            alt={products.title}
+          <Carousel
+            autoPlay="false"
+            navButtonsAlwaysVisible
             sx={{
               width: '100%',
-              maxHeight: '400px',
+              height: '400px',
               objectFit: 'cover',
               borderRadius: '10px',
             }}
-          />
+            next={(next, active) => console.log(`we left ${active}, and are now at ${next}`)}
+            prev={(prev, active) => console.log(`we left ${active}, and are now at ${prev}`)}>
+            {images.map((item, i) => (
+              <ImageModifier key={i} item={item} />
+            ))}
+          </Carousel>
 
           <Box
             sx={{
@@ -64,8 +80,7 @@ export const OneProductSection = () => {
               alignItems: 'center',
               textAlign: 'center',
               gap: 2,
-            }}
-          >
+            }}>
             <Typography variant="h4" component="h1" gutterBottom>
               {products.title}
             </Typography>
@@ -84,8 +99,7 @@ export const OneProductSection = () => {
                 width: '100%',
                 maxWidth: '300px',
                 gap: 2,
-              }}
-            >
+              }}>
               <CartButtonInPage productId={products.id} />
               <IconButton sx={{ color: theme.name === 'dark' ? 'white' : 'black' }}>
                 <FavoriteIcon />
