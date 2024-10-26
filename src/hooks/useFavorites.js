2a698efeb762c950f9457e39a224a89dbba4aa87
17sTomy <tomasignacioalv@hotmail.com>
2024-10-26 19:35:18 -0300

    feat: add useFavorite hook for getting favorites movies and toggle them
    Co-authored-by: Tobias Medina Viegas <tobiasmdn@gmail.com>
    Co-authored-by: Franco Baltasar Massi <francomassi1@gmail.com>

---
+++ src/hooks/useFavorites.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { getFavoriteProducts, toggleFavoriteProduct } from '../services/productsService';
+
+const useFavorites = () => {
+  const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchFavorites = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const favoritesData = await getFavoriteProducts();
+      setFavorites(favoritesData);
+    } catch (err) {
+      setError('Error al obtener los productos favoritos.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchFavorites();
+  }, [fetchFavorites]);
+
+  const handleToggleFavorite = async (id) => {
+    setLoading(true);
+    setError(null);
+    try {
+      await toggleFavoriteProduct(id);
+      fetchFavorites();
+    } catch (err) {
+      setError('Error al actualizar el estado de favorito del producto.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    favorites,
+    loading,
+    error,
+    handleToggleFavorite,
+  };
+};
+
+export default useFavorites;
