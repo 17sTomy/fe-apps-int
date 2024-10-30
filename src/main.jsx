0d0a240f2142c878abce7fcc06020f453966ec19
matiasgluck <matiasugluck@gmail.com>
2024-10-30 14:32:37 -0300

    feat: main entry point refactor to fix theme issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router/index.jsx';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
-
-import './index.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { Entrypoint } from './Entrypoint';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+  <Provider store={store}>
+    <Entrypoint />
+  </Provider>
 );
