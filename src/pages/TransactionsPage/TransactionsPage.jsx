bb22cc8cbe8a2e168c83aa3e562043d0bff48704
matiasgluck <matiasugluck@gmail.com>
2024-11-01 13:18:13 -0300

    fix: session error at transactions page

--- src/pages/TransactionsPage/TransactionsPage.jsx
+++ src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,7 +1,16 @@
 import { DashboardLayout } from '../../template/DashboardLayout';
 import TransactionHistory from '../../components/Transaction/TransactionHistory';
+import { RestrictedPage } from '../RestrictedPage';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const TransactionsPage = () => {
+  const accountStore = useSelector((state) => state.account);
+
+  if (!accountStore.authenticated) {
+    return <RestrictedPage />;
+  }
+
   return (
     <DashboardLayout>
       <TransactionHistory />
